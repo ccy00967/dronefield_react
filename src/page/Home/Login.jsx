7fde6a0a8aa234f26d8c1e85961cd476194a8ebd
Chae Chulyoung <ccy09671324@gmail.com>
2024-12-29 15:15:58 +0900

    fix

--- src/page/Home/Login.jsx
+++ src/page/Home/Login.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import axios from "axios";
+//import axios from "axios";
 import {
   GreenColor,
   hoverGreen,
